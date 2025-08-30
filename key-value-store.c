80006f78a488aa247bc7ec5dd89f4e8f42a856dd
Jason <jason.d.callaghan@gmail.com>
2025-08-29 22:04:38 -0700

    convert database to binary format

--- key-value-store.c
+++ key-value-store.c
@@ -14,95 +14,119 @@ void assert_equals_int(int expected, int actual) {
 	}
 }
 
-// return 1
+// return 1 if key not found
 // return -1 if an error occured
 // else return 0 and update value_out to the value
 int kv_get(void *key, size_t len, void **value_out, size_t *value_len_out) {
-	fptr = fopen(FILENAME, "r");
+	// printf("looking for key")
+
+	fptr = fopen(FILENAME, "rb");
 	if (fptr == NULL) {
 		return -1;
 	}
 
-	fseek(fptr, 0l, SEEK_END); // how do I get to the last line instead of the end of the file?
+	fseek(fptr, 0l, SEEK_END); // Go to end of file
 	long pos = ftell(fptr);
 	if (pos == 0) return 1; // file was empty
+	
 
-	// go to the start of the line, or start of the file
 while (pos > 0) {
+		fseek(fptr, - sizeof(size_t), SEEK_CUR);
 
-		// go to the start of the line
-		long line_start = 0;
-		for (long p = pos -1; p >= 0; p--) {
-			fseek(fptr, p, SEEK_SET);
-			int c = fgetc(fptr);
+		// get record length
+		size_t record_len; // maybe later check for max_key_len
+		fread(&record_len, sizeof(size_t), 1, fptr);
 
-			if (c == '\n') {
-				line_start = p + 1;
-				break;
+		printf("record len %zu\n", record_len);
+
+		fseek(fptr, -(record_len + sizeof(size_t)), SEEK_CUR); // go to the start of the record
+		
+		pos = ftell(fptr) - sizeof(size_t); // position is at the record_len of the last record now
+		printf("pos: %ld\n", pos);
+
+		char *buf = malloc(record_len);
+		if (buf == NULL) {
+			printf("malloc failed\n");
+			fclose(fptr);
+			return -1;
+		}
+
+		fread(buf, record_len, 1, fptr); // read entire record
+		fseek(fptr, -(record_len), SEEK_CUR); // go to the start of the record
+
+		printf("record len: %zu\n", record_len);
+
+		size_t key_len;
+		memcpy(&key_len, buf, sizeof(size_t));
+		
+		printf("size of read key: %zu\n size of requested key %zu\n", key_len, len);
+
+		char *key_ptr = buf + sizeof(size_t);
+
+		printf("set key ptr, about to enter if statement\n");
+
+		if (key_len == len && memcmp(key, key_ptr, len) == 0) {
+
+			printf("compared key\n");
+
+			size_t value_len;
+			char *value_len_ptr = key_ptr + key_len;
+			memcpy(&value_len, value_len_ptr, sizeof(size_t));
+
+			printf("copied value\n");
+
+			char *value_ptr = value_len_ptr + sizeof(size_t);
+			char *vbuf = malloc(value_len);
+
+			printf("malloc vbuf\n");
+
+			if (vbuf == NULL) {
+				fclose(fptr);
+				return -1;
 			}
-		}
+			memcpy(vbuf, value_ptr, value_len);
 
-		if (line_start == 0) {
-			fseek(fptr, 0, SEEK_SET);
-		} else {
-			fseek(fptr, line_start, SEEK_SET);
-		}
+			printf("copied value\n");
 
-		// check the key
-		char tmp_key[MAX_KEY_LEN];
-		size_t i = 0;
-		int c;
-		// read character until comma
-		// store all characters in a char array to later compare with the key param to this func
-		while ((c = fgetc(fptr)) != ',' && c != EOF && i < MAX_KEY_LEN - 1) {
-			tmp_key[i++] = c;
-		}
 
-		tmp_key[i] = '\0';
-
-		if (i == len && memcmp(key, tmp_key, len) == 0) {
-			long value_start = ftell(fptr);
-			long value_end = value_start;
-
-			while ((c = fgetc(fptr)) != '\n' && c != EOF) {
-				value_end++;
-			}
-
-			size_t value_len = value_end - value_start;
-
-			char *buf = malloc(value_len);
-			if (buf == NULL) return -1;
-
-			fseek(fptr, value_start, SEEK_SET);
-			fread(buf, 1, value_len, fptr); 
-			*value_out = buf;
+			*value_out = vbuf;
 			*value_len_out = value_len;
+			printf("Record length=%zu, key_len=%zu, value_len=%zu, key='%.*s'\n", 
+		  record_len, key_len, value_len, (int)key_len, key_ptr);
+
+			free(buf);
 
 			fclose(fptr);
 			return 0;
 		}
 
-		pos = line_start - 1;
+		printf("xyx\n");
+		free(buf);
 
+		printf("zyx\n");
 	}
+
 	fclose(fptr);
 	return -1;
 }
 
 int kv_set(void* key, size_t key_len, void* value, size_t value_len) {
-	fptr = fopen(FILENAME, "a");
-	size_t line_size = key_len + value_len;
+	fptr = fopen(FILENAME, "ab");
 
-	size_t total_size = key_len + strlen(",") + value_len;
 
-	size_t val1 = fwrite(key, 1, key_len, fptr);
-	if (val1 == 0) return -1;
-	size_t val2 = fwrite(",", 1, strlen(","), fptr);
-	if (val2 == 0) return -1;
-	size_t val3 = fwrite(value, 1, value_len, fptr);
-	if (val3 == 0) return -1;
-	size_t val4 = fwrite("\n", 1, strlen("\n"), fptr);
-	if (val4 == 0) return -1;
+	if (fwrite(&key_len, sizeof(size_t), 1, fptr) != 1) return -1;
+
+	if (fwrite(key, 1, key_len, fptr) != key_len) return -1;
+
+	if (fwrite(&value_len, sizeof(size_t), 1, fptr) != 1) return -1;
+
+	if (fwrite(value, 1, value_len, fptr) != value_len) return -1;
+
+	size_t record_len = key_len + value_len + 2 * sizeof(size_t);
+
+	if (fwrite(&record_len, sizeof(size_t), 1, fptr) != 1) return -1;
+
+	printf("Wrote key_len=%zu, key=%.*s, value_len=%zu, record_len=%zu\n", key_len, (int)key_len, (char*)key, value_len, record_len);
 
 	fclose(fptr);
 	return 0;
@@ -138,8 +162,6 @@ int main(int argc, char *argv[]) {
 	kv_get(key1, strlen(key1), &retrieved1, &retrieved1_len);
 
 	assert_equals_int(7, *(int *) retrieved1);
-
-
 
 	assert_equals_int(value1, *(int *) retrieved);
 
